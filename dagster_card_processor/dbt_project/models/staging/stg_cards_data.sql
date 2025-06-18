15151ef76e82ea5029b3dbbfd3c3b103bdde1632
Betsalel Williamson <saul.williamson@ieee.org>
2025-06-18 12:25:16 -0700

    add sample seed data for ci pipeline

--- dagster_card_processor/dbt_project/models/staging/stg_cards_data.sql
+++ dagster_card_processor/dbt_project/models/staging/stg_cards_data.sql
@@ -1,5 +1,19 @@
--- This model now reads the JSON file and loads it into the table.
--- The file path is passed in as a variable from the Dagster asset.
+-- models/staging/stg_cards_data.sql
+
+-- In a CI environment, dbt will be run with the 'is_ci_run' variable set to true.
+-- In that case, we select from the sample data provided by the seed file.
+{% if var('is_ci_run', default=false) %}
+
+select
+    *
+from {{ ref('stg_cards_data_sample') }}
+
+-- In a production run (triggered by Dagster), the 'is_ci_run' variable will be false or undefined.
+-- In that case, we select from the JSON file whose path is passed in by the Dagster asset.
+{% else %}
+
 select
     *
 from read_json_auto('{{ var("validated_json_path") }}')
+
+{% endif %}
