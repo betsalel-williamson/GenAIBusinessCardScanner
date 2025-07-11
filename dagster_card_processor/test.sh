94aa1bfe047abf1d45373a85f26dfabdcae24f61
Betsalel Williamson <saul.williamson@ieee.org>
2025-07-11 10:05:14 -0700

    always run from script root

--- dagster_card_processor/test.sh
+++ dagster_card_processor/test.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env sh
 
 set -e
+
+cd $(dirname "$0")
+
 set -a
 source .env.test
 set +a
 
 pytest \
-            --cov=dagster_card_processor \
-            --cov-report=xml:coverage.xml \
-            --junitxml=pytest-report.xml
+  --cov=dagster_card_processor \
+  --cov-report=xml:coverage.xml \
+  --junitxml=pytest-report.xml
 
 
